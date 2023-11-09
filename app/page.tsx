e600a74a84bb370a05fbc56350a445efe206c7cd
stephen Batelaan <sbatelaan91@gmail.com>
2023-11-08 18:24:06 -0800

    installed dependencies, set up auth and DB

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-lg py-5">Hello Next</h1>
+      
+    </main>
+  )
+}
---
+++ pages/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from '../../../prisma/client'
+
+
+export default NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+    ],
+  })
